38426d08fec4e0566b80dc3c20744a16355ae716
Jason Hemann <jason.hemann@gmail.com>
2021-09-07 10:59:49 -0400

    Correcting a typo and adding up the packages I need for class

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 apt-get update -y
-apt install software-properties-common -y
+apt-get install software-properties-common -y
 add-apt-repository ppa:plt/racket -y
 apt-get install -y racket
 apt-get clean
+raco setup --doc-index --force-user-docs
+raco pkg install -i --auto rackunit-abbrevs rebellion minikanren-ee sugar
+
